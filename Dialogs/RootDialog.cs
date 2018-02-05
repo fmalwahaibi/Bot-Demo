df8add2989bcee12fe9e67f5ad34ecbea4a341ee
fmalwahaibi <36155655+fmalwahaibi@users.noreply.github.com>
2018-02-05 15:17:02 +0400

    Add files via upload

---
+++ Dialogs/RootDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System.Data.SqlClient;
+
+namespace Bot_Application1.Dialogs
+{
+   
+
+    [LuisModel("72d3d5d5-df49-4a7c-8411-8da25ae33d6a", "6da30799363e4d3595ac34d404ad960b")]
+    [Serializable]
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+ 
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "Gretting" with the name of your newly created intent in the following handler
+        [LuisIntent("Greeting")]
+        public async Task GreetingIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Cancel")]
+        public async Task CancelIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Help")]
+        public async Task HelpIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        private async Task ShowLuisResult(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
+            context.Wait(MessageReceived);
+        }
+    }
+}
